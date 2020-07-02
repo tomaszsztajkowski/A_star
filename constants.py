819f3d482499834b56c807426e9ba3cf86d459a2
tomaszsztajkowski <tomaszsztajkowski@gmail.com>
2020-07-02 21:28:42 +0200

    modified:   __pycache__/constants.cpython-38.pyc 	modified:   constants.py

--- constants.py
+++ constants.py
@@ -3,3 +3,9 @@ t_size = 10
 border = 10
 win_size_x = 2 * border + size * t_size + size + 1
 win_size_y = 2 * border + size * t_size + size + 1
+
+euclidean = 14
+manhattan = 20
+search_type = 20
+
+calculated = 0
